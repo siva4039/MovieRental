3613a06bd17028d3209dbc79984a67044acebe50
sivakumar <52204776+siva4039@users.noreply.github.com>
2020-08-26 13:07:41 +0530

    ADDITION of controllers
    MOVIE and customers controllers along with their views are added

---
+++ MovieRental/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Ajax.Utilities;
+using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return Content("No customer detail found with the given id");
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "sivakumar"},
+                new Customer { Id = 2, Name = "srinivasarao"}
+            };
+
+        }
+    }
+}
---
+++ MovieRental/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            if(movie == null)
+            {
+                return Content("No movie found with the given id");
+            }
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{ Id = 1, Name = "DARBAR"},
+                new Movie{ Id = 2, Name = "PETTA"}
+            };
+
+        }
+
+    }
+}
